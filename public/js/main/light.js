ddebf3382f977a8d90aae3043003ef8b28cf62ed
Joe Munene <joemunene91@gmail.com>
2024-01-04 13:19:27 +0000

    h

--- public/js/main/light.js
+++ public/js/main/light.js
@@ -199,36 +199,6 @@
         });
 
 
-        function toggleFullScreen() {
-            if (!document.fullscreenElement && !document.mozFullScreenElement && !document.webkitFullscreenElement && !document.msFullscreenElement) {
-                if (document.documentElement.requestFullscreen) {
-                    document.documentElement.requestFullscreen();
-                } else if (document.documentElement.msRequestFullscreen) {
-                    document.documentElement.msRequestFullscreen();
-                } else if (document.documentElement.mozRequestFullScreen) {
-                    document.documentElement.mozRequestFullScreen();
-                } else if (document.documentElement.webkitRequestFullscreen) {
-                    document.documentElement.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
-                }
-            } else {
-                if (document.exitFullscreen) {
-                    document.exitFullscreen();
-                } else if (document.msExitFullscreen) {
-                    document.msExitFullscreen();
-                } else if (document.mozCancelFullScreen) {
-                    document.mozCancelFullScreen();
-                } else if (document.webkitExitFullscreen) {
-                    document.webkitExitFullscreen();
-                }
-            }
-        }
-        ;var navbar_init = function() {
-            $('.switcher-setting').on('click', function() {
-                toggleFullScreen();
-                return false;
-            });
-        };
-        navbar_init();
 
     });
 }
